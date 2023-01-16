34a3e2ed1b63dc3a4c524b72f877c1f8946f61f6
PabloMateosP <pmatpal0105@g.educaand.es>
2023-01-16 10:50:59 +0100

    Primera prueba arrays

--- src/Arrays/Practica/EjemploArray.java
+++ src/Arrays/Practica/EjemploArray.java
@@ -3,8 +3,8 @@ package Arrays.Practica;
 public class EjemploArray {
     public static void main(String[] args) {
         int [] a = {1,2,3,4,5};
-        System.out.println(a); //De esta forma imprimirá la refernrecia
-        System.out.println(a[0]);
+        System.out.println(a); // De esta forma imprimirá la refernrecia
+        System.out.println(a[0]); // Siempre tenemos que recordar que empezamos desde el 0
         System.out.println(a[1]);
         System.out.println(a[2]);
         System.out.println(a[3]);
