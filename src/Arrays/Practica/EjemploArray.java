4dab1e72c85853dbe44e2c3ab4c4bd4564aa1230
PabloMateosP <pmatpal0105@g.educaand.es>
2023-01-16 10:47:52 +0100

    Primera prueba arrays

---
+++ src/Arrays/Practica/EjemploArray.java
@@ -0,0 +1,13 @@
+package Arrays.Practica;
+
+public class EjemploArray {
+    public static void main(String[] args) {
+        int [] a = {1,2,3,4,5};
+        System.out.println(a); //De esta forma imprimirá la refernrecia
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+    }
+}
