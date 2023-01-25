69ac8b8752d74aea2d1d88d590dde3137d78faad
PabloMateosP <pmatpal0105@g.educaand.es>
2023-01-25 17:46:43 +0100

    actualización clase persona

--- src/Arrays/Practica/Persona.java
+++ src/Arrays/Practica/Persona.java
@@ -3,5 +3,16 @@ package Arrays.Practica;
 public class Persona {
     String name;
     int edad;
+    Persona (String name) {
+        this.name = name;
+    }
+
+    Persona() {}
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
 }
 
